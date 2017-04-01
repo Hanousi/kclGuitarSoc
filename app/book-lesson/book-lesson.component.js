9835120e4ccfead36c1e693d4db743dc7450b28e
Hanousi <tawil.hani@gmail.com>
2017-04-01 23:50:56 +0100

    Added a favicon to the website and started working on the calendar.

--- app/book-lesson/book-lesson.component.js
+++ app/book-lesson/book-lesson.component.js
@@ -6,7 +6,15 @@ component('bookLesson', {
     templateUrl: 'book-lesson/book-lesson.template.html',
     controller: function BookLessonController($scope, $http) {
         var self = this;
-        console.log('hi');
-        this.greeting = "Book a lesson";
+
+        this.daysInMonth = 30;
+        this.paddingMonth = 3;
+        this.weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+        this.getNumber = function getNumber(num) {
+            return new Array(num);
+        }
+        
+        console.log(this.getNumber(this.number));
     }
 });
